19629fd20e2d76cf3974fd2995a7ba972fe5d024
Matheus Bordin <mabordin98@gmail.com>
2019-02-13 20:47:43 -0200

    Remove front import

--- get-project.sh
+++ get-project.sh
@@ -4,8 +4,3 @@ git clone https://github.com/MatheusBordin/teravoz-api.git
     cd teravoz-api
     yarn
 )
-git clone https://github.com/MatheusBordin/teravoz-front.git
-{
-    cd teravoz-front
-    yarn
-}
